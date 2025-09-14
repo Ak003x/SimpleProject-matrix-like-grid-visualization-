66286af19e69e29dff443f1076830c6cd367f498
Dev_Ak <aakash.velu.003@gmail.com>
2025-09-14 16:10:23 +0530

    configure files in next.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+  output: 'export', // Enable static export
+  basePath: isProd ? '/SimpleProject-matrix-like-grid-visualization-' : '', // Replace with your repo name
+  trailingSlash: true,
+  images: {
+    unoptimized: true, // Required for static export
+  },
+};
 
 export default nextConfig;
+
